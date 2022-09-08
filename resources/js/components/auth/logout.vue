ced657b38937451416ae65853d6afca5711da376
kajal <kajalmorabiya@gmail.com>
2022-09-08 18:27:06 +0530

    initial commit

---
+++ resources/js/components/auth/logout.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    created() {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        Toast.fire({
+            icon: 'success',
+            title: 'Logout successfully'
+        })
+        this.$router.push({
+            name: '/'
+        })
+
+    }
+
+}
+</script>
+
+<style type="text/css">
+
+</style>
